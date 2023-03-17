56cbe9abca3f03593ee281b7f93fd8b589456f07
Aysun <aysunkapusuz@gmail.com>
2023-03-17 08:53:08 -0600

    exceptions

--- exceptions.py
+++ exceptions.py
@@ -34,3 +34,89 @@ except ValueError:
     print("x is not an integer")
 else:
     print(f"x is {x}")
+
+
+
+while True:
+          try:
+              x = int(input("What's x? ")) 
+          except ValueError:
+              print("x is not an integer")
+          else:
+              break
+          
+print(f"x is {x}")
+
+
+
+
+def main():
+     x = get_int()
+     print(f"x is {x}")
+
+def get_int():    
+    while True:
+              try:
+                  x = int(input("What's x? ")) 
+              except ValueError:
+                  print("x is not an integer")
+              else:
+                  break
+              return x
+
+main()
+
+
+
+
+
+def main():
+     x = get_int()
+     print(f"x is {x}")
+
+def get_int():    
+    while True:
+              try:
+                  return int(input("What's x? ")) 
+              except ValueError:
+                  print("x is not an integer")
+
+main()
+
+# pass  # silently ignoring 
+
+def main():
+     x = get_int()
+     print(f"x is {x}")
+
+def get_int():    
+    while True:
+              try:
+                  return int(input("What's x? ")) 
+              except ValueError:
+                  pass
+
+main()
+
+
+
+def main():
+     x = get_int("What's x? ")
+     print(f"x is {x}")
+
+def get_int(prompt):    
+    while True:
+              try:
+                  return int(input(prompt)) 
+              except ValueError:
+                  pass
+
+main()
+
+
+# raise keyword
+
+
+
+
+
